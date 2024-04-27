b6fcdba5fc53ab96fcd1480f8cbb7aa0f2095d36
nazzllsln <nazl.sln16@gmail.com>
2024-04-27 12:13:56 +0300

    güncelleme

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,15 +9,11 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-              // Arama işlemi burada gerçekleştirilebilir
-            },
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.arrow_forward),
-            onPressed: () {
-              // Diğer sayfaya geçiş işlemi burada gerçekleştirilebilir
-            },
+            onPressed: () {},
           ),
         ],
       ),
@@ -44,41 +40,62 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: GridView.builder(
-              itemCount: 8,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              children: List.generate(
+                8,
+                (index) => ProductItem(
+                  image:
+                      "https://cdn.dsmcdn.com/mnresize/1200/1800/ty456/product/media/images/20220618/14/127367013/502692412/1/1_org_zoom.jpg",
+                  tutar: "100 TL",
+                  name: "Ürün ${index + 1}",
+                ),
               ),
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.network(
-                          "https://cdn.dsmcdn.com/mnresize/1200/1800/ty456/product/media/images/20220618/14/127367013/502692412/1/1_org_zoom.jpg",
-                          width: 200,
-                        ),
-                        Icon(Icons.shopping_bag, size: 40.0),
-                        SizedBox(height: 10.0),
-                        Text('Ürün Adı'),
-                        Text('Ürün Fiyatı'),
-                      ],
-                    ),
-                  ),
-                );
-              },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final String image;
+  final String tutar;
+  final String name;
+
+  const ProductItem({
+    Key? key,
+    required this.image,
+    required this.tutar,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              image,
+              width: 200,
+            ),
+            Icon(Icons.shopping_bag, size: 40.0),
+            SizedBox(height: 10.0),
+            Text(name),
+            Text(tutar),
+          ],
+        ),
       ),
     );
   }
@@ -109,11 +126,4 @@ class RoundedCategoryIcon extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-  ));
 }
